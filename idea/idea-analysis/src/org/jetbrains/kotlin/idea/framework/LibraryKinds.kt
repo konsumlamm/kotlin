ef156b281a06952184f7ab175c088cfcf6d6d870
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2020-04-13 13:24:01 +0700

    Minor. Add comments

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/framework/LibraryKinds.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/framework/LibraryKinds.kt
@@ -42,6 +42,8 @@ import java.util.jar.Attributes
 import java.util.regex.Pattern
 
 interface KotlinLibraryKind {
+    // TODO: Drop this property. See https://youtrack.jetbrains.com/issue/KT-38233
+    //  This property returns approximate library platform, as the real platform can be evaluated only for concrete library.
     val compilerPlatform: TargetPlatform
 }
 
@@ -59,6 +61,8 @@ object CommonLibraryKind : PersistentLibraryKind<DummyLibraryProperties>("kotlin
     override fun createDefaultProperties() = DummyLibraryProperties.INSTANCE!!
 }
 
+// TODO: Drop this property. See https://youtrack.jetbrains.com/issue/KT-38233
+//  It returns approximate library platform, as the real platform can be evaluated only for concrete library.
 val PersistentLibraryKind<*>?.platform: TargetPlatform
     get() = when (this) {
         is KotlinLibraryKind -> this.compilerPlatform
